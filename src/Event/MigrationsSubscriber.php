cd15daf972677cb95f553b0e89e6bb14dd9eef20
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-06-25 19:03:39 +0430

    Renamed MigrationsListener to MigrationsSubscriber

--- src/Event/MigrationsSubscriber.php
+++ src/Event/MigrationsSubscriber.php
@@ -2,30 +2,43 @@
 
 namespace RadBundle\Migrations\Event;
 
-use Rad\DependencyInjection\Di;
-use Rad\Event\EventListener;
+use Rad\Application;
+use Rad\Events\EventManager;
+use Rad\Events\EventSubscriberInterface;
 use Rad\Routing\Router;
 use RadBundle\Migrations\Command\Create;
 use RadBundle\Migrations\Command\Migrate;
 use RadBundle\Migrations\Command\Rollback;
 use RadBundle\Migrations\Command\Status;
-use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Application as ConsoleApplication;
 
 /**
  * Migrations Event
  *
  * @package RadBundle\Migrations\Event
  */
-class MigrationsListener extends EventListener
+class MigrationsListener implements EventSubscriberInterface
 {
+    /**
+     * Subscribe event listener
+     *
+     * @param EventManager $eventManager
+     *
+     * @return mixed
+     */
+    public function subscribe(EventManager $eventManager)
+    {
+        $eventManager->attach(Application::EVENT_AFTER_CLI_METHOD, 'runConsoleApplication');
+    }
+
     /**
      * Run console application
      *
-     * @param Di $di
+     * @param EventManager $event
      *
      * @throws \Rad\DependencyInjection\Exception
      */
-    public function runConsoleApplication(Di $di)
+    public function runConsoleApplication(EventManager $event)
     {
         /** @var Router $router */
         $router = $di->get('router');
@@ -44,7 +57,7 @@ class MigrationsListener extends EventListener
             $_SERVER['argv'] = array_merge(explode(':', $path), $_SERVER['argv']);
         }
 
-        $app = new Application('Migrations plugin, based on Phinx by Rob Morgan.', PHINX_VERSION);
+        $app = new ConsoleApplication('Migrations plugin, based on Phinx by Rob Morgan.', PHINX_VERSION);
         $app->add(new Status());
         $app->add(new Create());
         $app->add(new Migrate());
