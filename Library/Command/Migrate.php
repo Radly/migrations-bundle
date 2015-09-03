ab92ff06bba6d05b12d177c0d6de408be8ed89f4
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-09-03 12:08:10 +0430

    Add new date option

--- Library/Command/Migrate.php
+++ Library/Command/Migrate.php
@@ -4,7 +4,7 @@ namespace Migrations\Library\Command;
 
 use Phinx\Console\Command\Migrate as MigrateCommand;
 use Migrations\Library\ConfigurationTrait;
-use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * Migrate Command
@@ -22,8 +22,19 @@ class Migrate extends MigrateCommand
     {
         $this->setName('migrate')
             ->setDescription('Migrate the database')
-            ->addOption('--target', '-t', InputArgument::OPTIONAL, 'The version number to migrate to')
-            ->setHelp('runs all available migrations, optionally up to a specific version')
-            ->addOption('--bundle', '-b', InputArgument::OPTIONAL, 'The bundle containing the migrations');
+            ->addOption('--target', '-t', InputOption::VALUE_REQUIRED, 'The version number to migrate to')
+            ->addOption('--date', '-d', InputOption::VALUE_REQUIRED, 'The date to migrate to')
+            ->addOption('--bundle', '-b', InputOption::VALUE_OPTIONAL, 'The bundle containing the migrations')
+            ->setHelp(
+                <<<EOT
+ The <info>migrate</info> command runs all available migrations, optionally up to a specific version
+
+<info>migrations:migrate</info>
+<info>migrations:migrate -t 20110103081132</info>
+<info>migrations:migrate -d 20110103</info>
+<info>migrations:migrate -v</info>
+
+EOT
+            );
     }
 }
