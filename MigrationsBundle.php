21425912e522467b0bc724eb3f9352a31920f6c6
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-09-07 22:58:39 +0430

    Rename Bootstrap to MigrationsBundle

---
+++ MigrationsBundle.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Migrations;
+
+use Rad\Configure\Config;
+use Rad\Core\AbstractBundle;
+use Migrations\Event\MigrationsSubscriber;
+
+/**
+ * Migrations Bundle
+ *
+ * @package Migrations
+ */
+class MigrationsBundle extends AbstractBundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function startup()
+    {
+        $this->getEventManager()->addSubscriber(new MigrationsSubscriber());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loadConfig()
+    {
+        Config::load(__DIR__ . DS . 'Config' . DS . 'config.php');
+    }
+}
